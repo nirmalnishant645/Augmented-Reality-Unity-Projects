b51385670bb35c84aacf4e2eb2492ac68648bcec
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-25 23:42:17 +0530

    Assets

---
+++ AR Project 6 - AR Pikachu on User Defined Target/Assets/HealthManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+	public int maxHealth;
+	public int currentHealth;
+
+	public PikachuAnimation thePlayer;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;	
+		
+		thePlayer = FindObjectOfType<PikachuAnimation>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void HurtPlayer(int damage)
+	{
+		currentHealth -= damage;
+	
+	}
+
+	public void HealPlayer(int healAmount)
+	{
+		currentHealth += healAmount;
+		
+		if (currentHealth > maxHealth)
+		{
+			currentHealth = maxHealth;
+		}
+	}
+}
---
+++ AR Project 6 - AR Pikachu on User Defined Target/Assets/PikachuAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PikachuAnimation : MonoBehaviour {
+
+	private Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+
+		Anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	public void AnimStop() {
+		Anim.Play("Stop", -1, 0f);
+		Anim.speed = 1f;
+	}	
+	
+	public void AnimForward() {
+
+		Anim.Play("Forward", -1, 0f);
+		Anim.speed = 1f;
+	}
+
+	public void AnimLeft() {
+		Anim.Play("Left", -1, 0f);
+		Anim.speed = 1f;
+	}
+
+	public void AnimRight() {
+		Anim.Play("Right", -1, 0f);
+		Anim.speed = 1f;
+	}
+
+}
